73b8be09434cedbb7e242bdd43ff6ec4f296af00
xinli <xiaoxinlixxl@gmail.com>
2017-12-17 01:05:27 -0800

    first try

---
+++ symbolcal.py
@@ -0,0 +1,18 @@
+from __future__ import division
+from sympy import *
+# from sympy.abc import x
+
+x,y,z,t=symbols('x y z t')
+
+expr=(1/(1-x))**3
+
+
+m=10
+
+f=fps(expr,x).truncate(m+1)
+g=diff(expr,x,m)
+numb=g.evalf(subs={x:0})/factorial(m)
+
+
+print(f)
+print(numb)
